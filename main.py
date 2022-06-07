6109cc40feaa5caaa0b077554b99d33f4a1e479f
MARX1108 <boyuan@vt.edu>
2022-06-07 11:59:13 -0400

    base

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import rpy2
+
+from rpy2.robjects.packages import importr
+# import R's "base" package
+base = importr('base')
+# import R's "utils" package
+utils = importr('utils')
 
 brain = pd.read_csv("./data/brain.csv")
 main = pd.read_csv("./data/data.csv")
